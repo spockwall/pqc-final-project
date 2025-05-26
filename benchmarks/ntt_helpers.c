52817c02ec01255bc76326e62e92b484a410bf7c
spockwall <abcdefg35216874@gmail.com>
2025-05-26 21:34:09 +0800

    feat: add helper functions - is_power_of_2 - logn

--- benchmarks/ntt_helpers.c
+++ benchmarks/ntt_helpers.c
@@ -22,15 +22,31 @@ static inline uint32_t montmul(uint32_t a, uint32_t b)
     return (u >= Q) ? u - Q : (uint32_t)u;
 }
 
-static uint32_t pow_mod(uint32_t g_, uint32_t e)
+static inline uint32_t pow_mod(uint32_t g, uint32_t e)
 {
     uint32_t r = 1;
     while (e)
     {
         if (e & 1)
-            r = montmul(r, g_);
-        g_ = montmul(g_, g_);
+            r = montmul(r, g);
+        g = montmul(g, g);
         e >>= 1;
     }
     return r;
+}
+
+static inline int is_power_of_2(uint32_t x)
+{
+    return (x != 0) && ((x & (x - 1)) == 0);
+}
+
+static inline unsigned log2n(uint32_t n)
+{
+    unsigned log = 0;
+    while (n > 1)
+    {
+        n >>= 1;
+        log++;
+    }
+    return log;
 }
--- benchmarks/ntt_helpers.h
+++ benchmarks/ntt_helpers.h
@@ -6,4 +6,10 @@ static inline uint32_t sub_mod(uint32_t a, uint32_t b);
 static inline uint32_t montmul(uint32_t a, uint32_t b);
 
 // 32×32-bit Montgomery multiply, branch-free
-static uint32_t pow_mod(uint32_t g_, uint32_t e);
+static inline uint32_t pow_mod(uint32_t g_, uint32_t e);
+
+// check an uint32_t value is a power of 2
+static inline int is_power_of_2(uint32_t x);
+
+// compute log n
+static inline unsigned log2n(uint32_t n);
